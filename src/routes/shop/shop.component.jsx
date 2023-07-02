7f97b65b2d3c657a69a05966f38e2ec4536f0553
Shivank Mitra <shivankcode@gmail.com>
2023-07-02 19:34:13 +0530

    Create product card component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,17 @@
 import { useContext } from "react"
 import { ProductsContext } from "../../contexts/products.context"
 
+import './shop.styles.scss'
+
+import ProductCard from "../../components/product-card/product-card.component"
+
 function Shop() {
     const {products} = useContext(ProductsContext)
 
     return (
-        <div>
-            {products.map(({id, name}) => (
-                <div key={id}>{name}</div>
+        <div className="products-container">
+            {products.map(product => (
+                <ProductCard key={product.id} product={product} />
             ))}
         </div>
     )
