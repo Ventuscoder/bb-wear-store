7b204d211f29012332227c205377963db436d4fe
Shivank Mitra <shivankcode@gmail.com>
2023-07-10 15:30:06 +0530

    Add products collection to firestore documents

---
+++ src/shop-data.js
@@ -0,0 +1,94 @@
+const SHOP_DATA = [
+    {
+        title: 'Jordan',
+        location: 'aj',
+        items: [
+            {
+              "id": 1,
+              "name": "Jumpman MVP",
+              "price": 60
+            },
+            {
+              "id": 2,
+              "name": "Max Aura 4",
+              "price": 40
+            },
+            {
+              "id": 3,
+              "name": "Air Jordan 1 Mid",
+              "price": 45
+            },
+            {
+              "id": 4,
+              "name": "Air Jordan XXXVII Low PF",
+              "price": 75
+            }
+        ]          
+    },
+    {
+        title: 'KD',
+        location: 'kd',
+        items: [
+            {
+                "id": 5,
+                "name": "KD Trey 5",
+                "price": 80
+            },
+            {
+                "id": 6,
+                "name": "KD 15 EP",
+                "price": 35
+            }
+        ]
+    },
+    {
+        title: 'LeBron',
+        location: 'lb',
+        items: [
+            {
+                "id": 7,
+                "name": "LeBron Witness 7",
+                "price": 70
+            },
+            {
+                "id": 8,
+                "name": "LeBron NXXT Gen EP",
+                "price": 50
+            }
+        ]
+    },
+    {
+        title: 'Curry',
+        location: 'cu',
+        items: [
+            {
+                "id": 9,
+                "name": "Curry Flow 10",
+                "price": 70
+            },
+            {
+                "id": 10,
+                "name": "Curry Splash 2",
+                "price": 30
+            }
+        ]
+    },
+    {
+        title: 'Giannis',
+        location: 'gi',
+        items: [
+            {
+                "id": 11,
+                "name": "Giannis Immortality 2 SE",
+                "price": 95
+            },
+            {
+                "id": 12,
+                "name": "Freak 4",
+                "price": 55
+            }
+        ]
+    }
+]
+
+export default SHOP_DATA
