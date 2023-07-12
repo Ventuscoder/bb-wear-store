6f092f51145b79b10145eb14d0a6b9f0e89d55ee
Shivank Mitra <shivankcode@gmail.com>
2023-07-12 20:16:49 +0530

    Use reducer for user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react"
+import { createContext, useEffect, useReducer } from "react"
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils"
 
@@ -7,8 +7,35 @@ export const UserContext = createContext({
     setCurrentUser: () => null
 })
 
+export const userActionTypes = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+function userReducer(state, action) {
+    const { type, payload } = action
+
+    switch (type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`)
+    }
+}
+
+const initialState = {
+    currentUser: null
+}
+
 export function UserProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState(null)
+    const [{ currentUser }, dispatch] = useReducer(userReducer, initialState)
+
+    function setCurrentUser(user) {
+        dispatch({ type: userActionTypes.SET_CURRENT_USER, payload: user })
+    }
+
     const value = { currentUser, setCurrentUser }
 
     useEffect(() => {
